6adc2a2168ed4b4be05aae8e7c3917d4795228ea
Makoto Yamazaki <makoto1975@gmail.com>
2016-08-06 11:27:55 +0900

    「9.重複排除と時間順での並べ替え」完了状態

--- Twitter/app/src/main/java/io/realm/handson4/twitter/TimelineFragment.java
+++ Twitter/app/src/main/java/io/realm/handson4/twitter/TimelineFragment.java
@@ -1,6 +1,7 @@
 package io.realm.handson4.twitter;
 
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ListFragment;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,6 +10,7 @@ import android.widget.TextView;
 import io.realm.Realm;
 import io.realm.RealmBaseAdapter;
 import io.realm.RealmResults;
+import io.realm.Sort;
 import io.realm.handson4.twitter.entity.Tweet;
 
 public class TimelineFragment extends ListFragment {
@@ -20,7 +22,7 @@ public class TimelineFragment extends ListFragment {
 
         realm = Realm.getDefaultInstance();
 
-        final RealmResults<Tweet> tweets = realm.where(Tweet.class).findAll();
+        final RealmResults<Tweet> tweets = buildTweetList(realm);
         final RealmBaseAdapter<Tweet> adapter = new RealmBaseAdapter<Tweet>(getContext(), tweets) {
             @Override
             public View getView(int position, View convertView, ViewGroup parent) {
@@ -39,6 +41,11 @@ public class TimelineFragment extends ListFragment {
         };
 
         setListAdapter(adapter);
+    }
+
+    @NonNull
+    protected RealmResults<Tweet> buildTweetList(Realm realm) {
+        return realm.where(Tweet.class).findAllSorted("createdAt", Sort.DESCENDING);
     }
 
     @Override
