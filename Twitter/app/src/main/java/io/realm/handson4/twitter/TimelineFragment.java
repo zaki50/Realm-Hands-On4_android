8aa45ffe163049f287dd3da4aaf8f9b70f386d51
Makoto Yamazaki <makoto1975@gmail.com>
2016-08-06 11:23:52 +0900

    「8.Realmからの読み込み」完了状態

--- Twitter/app/src/main/java/io/realm/handson4/twitter/TimelineFragment.java
+++ Twitter/app/src/main/java/io/realm/handson4/twitter/TimelineFragment.java
@@ -1,44 +1,51 @@
 package io.realm.handson4.twitter;
 
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ListFragment;
-import android.widget.ArrayAdapter;
-import android.widget.ListAdapter;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
 
-import java.util.Arrays;
+import io.realm.Realm;
+import io.realm.RealmBaseAdapter;
+import io.realm.RealmResults;
+import io.realm.handson4.twitter.entity.Tweet;
 
 public class TimelineFragment extends ListFragment {
+    private Realm realm;
 
     @Override
-    public void onCreate(@Nullable Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-        final ListAdapter adapter = new ArrayAdapter<String>(getContext(),
-                R.layout.listitem_tweet,
-                R.id.text,
-                Arrays.asList("Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
-                        "Lorem Ipsum is simply dummy text of the printing and typesetting industry."
-                ));
+        realm = Realm.getDefaultInstance();
+
+        final RealmResults<Tweet> tweets = realm.where(Tweet.class).findAll();
+        final RealmBaseAdapter<Tweet> adapter = new RealmBaseAdapter<Tweet>(getContext(), tweets) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                final Tweet tweet = getItem(position);
+
+                if (convertView == null) {
+                    convertView = inflater.inflate(R.layout.listitem_tweet, parent, false);
+                }
+
+                // TODO 余裕があればViewHolderパターンを適用してください
+                ((TextView) convertView.findViewById(R.id.screen_name)).setText(tweet.getScreenName());
+                ((TextView) convertView.findViewById(R.id.text)).setText(tweet.getText());
+
+                return convertView;
+            }
+        };
 
         setListAdapter(adapter);
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ((RealmBaseAdapter<?>) getListAdapter()).updateData(null);
+        realm.close();
+        realm = null;
     }
 }
